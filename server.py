7d6d2b2993f8f13ab7c1d4484c41ca00e4138a2a
Your Name <you@example.com>
2020-02-05 17:29:42 -0800

    Made Login

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import (Flask, render_template, redirect, request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -19,10 +19,66 @@ app.secret_key = "ABC"
 app.jinja_env.undefined = StrictUndefined
 
 
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+
+    return render_template("user_list.html", users=users)
+
+
+@app.route("/register", methods=['POST'])
+def register_process():
+    """register new people"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user_email = User.query.filter_by(email=email).first()
+
+    if user_email is None:
+        new_user=User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET"])
+def register_form():
+
+    return render_template("register_form.html")
+
+
+@app.route("/login", methods=['GET'])
+def login_form():
+
+    return render_template("login_form.html")
+
+
+@app.route("/login", methods=['POST'])
+def user_login():
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user_email = User.query.filter_by(email=email).first()
+    user_password = User.query.filter_by(password=password).first()
+
+    if user_email is None:
+
+        return redirect("/register")
+    
+    else:
+        if user_password == password:
+
+            return redirect("/")
+
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
 
 
 if __name__ == "__main__":
